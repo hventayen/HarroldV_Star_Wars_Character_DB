cd7bc04a33fe5f534d53e0042303d8bab574700a
hventayen <harrold.ventayen@gmail.com>
2025-05-13 09:48:37 -0700

    updates

--- star_wars_character_db/src/main.js
+++ star_wars_character_db/src/main.js
@@ -11,3 +11,17 @@ function displayDate() {
     // Get the value of the input
   document.getElementById("his").innerHTML = searchInput;
 }
+
+const results = document.getElementById("results");
+document.addEventListener("DOMContentLoaded", function(){
+  fetch(`https://swapi.py4e.com/api/people`).then(resp => resp.json()).then(data => {
+console.log(data)
+const listOfCharacterNames = data.results.map(character => {
+   return `<li>${character.name}</li>`
+});
+results.innerHTML = results.innerHTML = `<ul class="characters">${listOfCharacterNames}</ul>`;
+  }).catch(e => {
+console.log(e);
+results.innerText = "The characters you seek are not here";
+  })
+})
